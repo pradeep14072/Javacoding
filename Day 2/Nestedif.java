4c78fecb96ac15740d9fdfb0114c67c6baa97325
Pradeep K <pradeep20005888@gmail.com>
2025-02-04 11:55:39 +0530

    problem practiced

---
+++ Day 2/Nestedif.java
@@ -0,0 +1,21 @@
+public class Nestedif {
+    public static void main(String[] args) {
+            int a=60;
+            boolean hasticket=true;
+            if(hasticket==true){
+                if(a>=60)
+                {
+                    System.out.println("Eligible for Test");
+                }
+                else{
+                    System.out.println("Pay fine then go for Test");
+                }
+            }else{
+                System.out.println("Not Eligible for Test");
+            }
+    
+        }
+    }
+    
+             
+        
